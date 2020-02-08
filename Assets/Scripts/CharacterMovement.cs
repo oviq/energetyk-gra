7aa785a8db09142ae0050baa27a8000fd1d11f2a
oviq <57455821+oviq@users.noreply.github.com>
2020-02-08 01:33:20 +0100

    wszystko od nowa 1
    - chodzenie po mapie
    - kolizje
    - responsywny player controller (z grubsza)
    - renderowanie tilemap dziala (no prawie)
    - przelaczanie sie pomiedzy postaciami
    - placeholderowy system animacji

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector3 movement;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (Vector2)movement * speed * Time.fixedDeltaTime);
+    }
+
+    public void ApplyMovement(Vector3 _movement)
+    {
+        movement = _movement;
+
+        animator.SetFloat("speed", Mathf.Abs(movement.magnitude));
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 movement;
+
+    public GameObject boy;
+    public GameObject gal;
+
+    public CameraFollow cam;
+
+    private CharacterMovement controller;
+    private bool state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.target = boy;
+        controller = boy.GetComponent<CharacterMovement>();
+        state = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        controller.ApplyMovement(movement);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controller.ApplyMovement(Vector3.zero);
+
+            state = !state;
+
+            if (state == true)
+            {
+                cam.target = gal;
+                controller = gal.GetComponent<CharacterMovement>();
+            }
+
+            if (state == false)
+            {
+                cam.target = boy;
+                controller = boy.GetComponent<CharacterMovement>();
+            }
+        }
+    }
+}
