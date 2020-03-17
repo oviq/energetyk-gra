1dadb3ce2b5512f84ea2ddfe81bb7dc0bc550da8
oviq <mnkisala@gmail.com>
2020-03-17 13:58:04 +0100

    Przeciwnik AI
    Ten glut pierdolony potrafi atakowac, a takze korzysta z maszyny stanow

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum EnemyState {
+public enum EnemyState {
     IDLE,
     FOLLOW,
     ATTACK
@@ -13,21 +13,23 @@ public class EnemyAI : MonoBehaviour
 {
     CharacterPathfinding pathfinding;
     Unit unit;
+    Animator animator;
 
     public float sightDistance = 5f;
 
-    EnemyState state;
+    public EnemyState state;
 
     // Start is called before the first frame update
     void Start()
     {
         unit = GetComponent<Unit>();
         pathfinding = GetComponent<CharacterPathfinding>();
+        // ! Poki co skrypt korzysta z pustego animatora, ale kiedys przydaloby sie cos podpiac !
+        animator = new Animator();
 
         state = EnemyState.IDLE;
 
-        InvokeRepeating("UpdateState", 0f, 0.5f);
-        InvokeRepeating("LookForTargets", 0f, 0.6f);
+        InvokeRepeating("UpdateState", 0f, 0.3f);
     }
 
     // ta funkcja implementuje cala maszyne stanow
@@ -35,15 +37,57 @@ public class EnemyAI : MonoBehaviour
     {
         if (state == EnemyState.IDLE)
         {
-            
+            LookForTargets();
         }
         else if (state == EnemyState.FOLLOW)
         {
+            // jezeli nie ma celu to przechodzi w idle
+            if (unit.currentTarget == null)
+            {
+                state = EnemyState.IDLE;
+                return;
+            }
 
+            // sprawdzanie czy mozna wykonac atak
+            if (Vector3.Distance(gameObject.transform.position, unit.currentTarget.transform.position) <= unit.currentAttack.GetRange())
+            {
+                // jezeli tak to zmienia stan na atakowanie
+                state = EnemyState.ATTACK;
+                return;
+            }
         }
         else if (state == EnemyState.ATTACK)
         {
+            // jezeli nie ma celu to przechodzi w idle
+            if (unit.currentTarget == null)
+            {
+                Debug.Log("DUapa");
+                state = EnemyState.IDLE;
+            }
+        }
+    }
 
+    private void Update()
+    {
+        if (state == EnemyState.ATTACK)
+        {
+            // sprawdzanie czy mozna wykonac atak
+            if (unit.currentTarget != null)
+            {
+                if (unit.currentAttack != null)
+                {
+                    if (Vector3.Distance(gameObject.transform.position, unit.currentTarget.transform.position) <= unit.currentAttack.GetRange())
+                    {
+                        // jesli tak to atakuje
+                        unit.Attack(animator);
+                    }
+                    else // jak nie to wraca do podazania
+                    {
+                        state = EnemyState.FOLLOW;
+                        UpdateState();
+                    }
+                }
+            }
         }
     }
 
@@ -55,14 +99,21 @@ public class EnemyAI : MonoBehaviour
         {
             Collider2D[] colliders = Physics2D.OverlapCircleAll(gameObject.transform.position, sightDistance);
 
-            // ustawi obiekt jako cel, jesli jest jednostka kontrolowalna przez gracza
+            // ustawi obiekt jako cel, jesli jest jednostka kontrolowalna przez gracza, a takze ma wiecej niz 0 hp
             foreach (Collider2D x in colliders)
             {
                 if (x.gameObject.GetComponent<Unit>() != null)
                 {
                     if (x.gameObject.GetComponent<Unit>().canBeControlledByPlayer)
                     {
-                        pathfinding.SetTarget(x.transform);
+                        if (x.gameObject.GetComponent<Unit>().isAlive)
+                        {
+                            pathfinding.SetTarget(x.transform);
+                            unit.currentTarget = x.gameObject.GetComponent<Unit>();
+                            // zmienia stan maszyny stanow na podazanie
+                            state = EnemyState.FOLLOW;
+                            UpdateState();
+                        }
                     }
                 }
             }
---
+++ Assets/Scripts/attacks/BOZ.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOZ : Attack
+{
+    public override float GetCooldown()
+    {
+        return 1f;
+    }
+
+    public override float GetRange()
+    {
+        return 1.5f;
+    }
+
+    public override void Action(Unit target, Animator animator)
+    {
+        target.TakeDamage(10f);
+    }
+}
